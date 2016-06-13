1008d6a74d2dc5c27bb38909892607f575f3aed8
Gary Bezet <Gary@GarysCorner.NET>
2016-06-13 13:33:51 -0500

    Initial Commit

---
+++ codejamlastword.go
@@ -0,0 +1,9 @@
+//File:		codejamlastword.go
+//Author:	Gary Bezet
+//Date:		2016-06-13
+//Desc:		This program is designed to solve Google Code Jam "The Last Word"
+//Problem:	https://code.google.com/codejam/contest/4304486/dashboard
+
+
+
+
